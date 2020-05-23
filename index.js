b52adb0a737497aad3a265c58b98e23d262b1dcc
StormrageXC <610211409@qq.com>
2020-05-23 20:49:17 +0800

    db operation

--- index.js
+++ index.js
@@ -1,9 +1,52 @@
 const express = require('express'),
     app = express(),
-    port = process.env.PORT || 3000;
-app.get('/',(req, res) => {
-    res.send('start')
+    bodyParser = require('body-parser'),
+    read = require('node-readability'),
+    Article = require('./db').Article;
+app.set('port', process.env.PORT || 3000);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/', (req, res, next) => {
+    res.send('It is ok');
 });
-app.listen(port, () => {
-    console.log('server start')
+app.get('/articles', (req, res, next) => {
+    Article.all((err, articles) => {
+        if (err) return next(err);
+        res.send(articles);
+    });
+});
+app.get('/articles/:id', (req, res, next) => {
+    const id = req.params.id;
+    Article.find(id, (err, article) => {
+        if (err) return next(err);
+        res.send(article.content);
+    });
+});
+// CMD curl -X DELETE http://localhost:8080/articles/1
+app.delete('/articles/:id', (req, res, next) => {
+    const id = req.params.id;
+    Article.delete(id, (err) => {
+        if (err) return next(err);
+        res.send({ message: 'delete success' });
+    });
+});
+// CMD curl -d "url=http://manning.com/cantelon2/" http://localhost:8080/articles
+app.post('/articles', (req, res, next) => {
+    const url = req.body.url;
+    read(url, (err, req) => {
+        if (err || !req) res.status(500).send('ERROR LOADING');
+        Article.create({
+            title: req.title,
+            content: req.content
+        }, (err, article) => {
+            if (err) return next(err);
+            res.send(`${article} CREATE SUCCESS`);
+        })
+    })
+
 })
+app.listen(app.get('port'), () => {
+    console.log(`server start ${app.get('port')}`)
+})
+
+module.exports = app
